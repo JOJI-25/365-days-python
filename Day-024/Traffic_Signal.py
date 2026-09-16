857526722acf49b4b640dfff88b995c889702fcd
JOJI-25 <jojicg25@gmail.com>
2026-09-16 16:00:56 +0530

    Add Day 024 Traffic Signal solution

---
+++ Day-024/Traffic_Signal.py
@@ -0,0 +1,25 @@
+class TrafficSignal:
+    def __init__(self,location,color):
+        self.location = location
+        self.color = color
+
+    def change_color(self,new_color):
+        if new_color in ["Red", "Yellow", "Green"]:
+            self.color = new_color
+            print("Updated Color: ",self.color)
+        else:
+            print("Invalid")
+
+    def display(self):
+        print(f"Location: {self.location}")
+        print(f"Color: {self.color}")
+
+    
+trafic1 = TrafficSignal("Busy Junction","Red")
+trafic1.change_color("Green")
+trafic1.display()
+
+trafic2 = TrafficSignal("Main Road","Yellow")
+trafic2.change_color("Red")
+trafic2.display()
+
